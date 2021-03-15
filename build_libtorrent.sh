3e08407450def42ce55d6e2e24e4970d4969d8ff
feixiao <feixiao>
2021-03-15 10:32:14 +0800

    放弃toolchain编译

--- build_libtorrent.sh
+++ build_libtorrent.sh
@@ -4,30 +4,61 @@ set -eu
 DEST_ARCH=$1
 echo "DEST_ARCH=$DEST_ARCH"
 if [ -z "$DEST_ARCH" ]; then
-    echo "You must specific an architecture 'armv7a, x86, ...'."
+    echo "You must specific an architecture 'armv7a, x86, arm64, x86_64...'."
     echo ""
     exit 1
 fi
 
+if [ -z "$ANDROID_NDK" -o -z "$ANDROID_NDK" ]; then
+    echo "You must define ANDROID_NDK, ANDROID_SDK before starting."
+    echo "They must point to your NDK and SDK directories.\n"
+    exit 1
+fi
+
+
 ROOT_DIR=$PWD
 ARCHS_32="armv7a x86"
-ARCHS_64="armv7a arm64 x86 x86_64"
+ARCHS_64="arm64 x86_64"
 
 ARCH_TYPE=
-RANLIB_NAME=
-HOST_NAME=
+BUILD_NAME=
 BOOST_PREFIX=
+# 基础库目录
+DEPS_PREFIX=/home/frank/deps/mobile_deps
 
 if [[ "$DEST_ARCH" = "armv7a" ]]; then
     ARCH_TYPE="arm"
-    RANLIB_NAME="arm-linux-androideabi-ranlib"  
-    HOST_NAME="arm-linux-androideabi"
-    BOOST_PREFIX=/home/frank/deps/mobile_deps/armeabi-v7a
-    
-elif [[ "$DEST_ARCH" = "x86" ]]; then
-    ARCH_TYPE="x86"  
-    RANLIB_NAME="i686-linux-android-gcc-ranlib"
-    HOST_NAME="i686-linux-android-gcc"
+    # HOST_NAME="arm-linux-androideabi"
+    BUILD_NAME=armeabi-v7a
+    BOOST_PREFIX=${DEPS_PREFIX}/armeabi-v7a
+    CC=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang
+    CXX=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang++
+    PATH=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+# elif [[ "$DEST_ARCH" = "x86" ]]; then
+#     ARCH_TYPE="x86"  
+#     RANLIB_NAME="i686-linux-android-gcc-ranlib"
+#     HOST_NAME="i686-linux-android-gcc"
+#     BOOST_PREFIX=${DEPS_PREFIX}/x86
+elif [[ "$DEST_ARCH" = "arm64" ]]; then
+    ARCH_TYPE="arm64"  
+    # HOST_NAME="aarch64-linux-android-gcc"
+    BUILD_NAME=arm64-v8a
+    BOOST_PREFIX=${DEPS_PREFIX}/arm64-v8a
+    CC=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang
+    CXX=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang++
+    PATH=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+elif [[ "$DEST_ARCH" = "x86_64" ]]; then
+    ARCH_TYPE="x86_64"  
+    BUILD_NAME=x86_64
+    # HOST_NAME="x86_64-linux-android-gcc"
+    BOOST_PREFIX=${DEPS_PREFIX}/x86_64
+    CC=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang
+    CXX=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang++
+    PATH=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin:${PATH}
+else 
+    echo "You must specific an architecture 'armv7a, arm64, x86_64, ...'."
+    echo ""
+    exit 1    
 fi
 
 BOOST_VERSION=1.69.0
@@ -40,80 +71,17 @@ source lib_archive.sh
 #init_boost $BOOST_VERSION
 init_libtorrent $LIBTORRENT_BRANCH $LIBTORRENT_VERSION
 
-if [ -z "$ANDROID_NDK" -o -z "$ANDROID_NDK" ]; then
-    echo "You must define ANDROID_NDK, ANDROID_SDK before starting."
-    echo "They must point to your NDK and SDK directories.\n"
-    exit 1
-fi
-
-TOOLCHAIN=$PWD/toolchain-$ARCH_TYPE
-export ANDROID_TOOLCHAIN=$TOOLCHAIN
-# BOOST_PREFIX=${TOOLCHAIN}/build
-
-if [ ! -d "$TOOLCHAIN" ]; then
-  echo "Creating toolchain..."
-  $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
-      --platform=android-21 \
-      --install-dir="$TOOLCHAIN" \
-      --arch="$ARCH_TYPE" \
-      --use-llvm \
-      --stl=libc++
-else
-  echo "Toolchain already created"
-fi
-
-#echo "Building boost_$BOOST_VERSION"
-
-# cd $BOOST_DIR
-# echo "Copy user-config.jam"
-# USER_CONFIG=$ROOT_DIR/$BOOST_DIR/tools/build/src/user-config.jam
-# rm -f $USER_CONFIG
-# cp $ROOT_DIR/config/android-$ARCH_TYPE-config.jam $USER_CONFIG
-
-# echo "Bootstrapping..."
-# ./bootstrap.sh --prefix=${BOOST_PREFIX}
-
-# echo "Building..."
-# ./b2 install \
-#     -j8 \
-#     --with-atomic \
-#     --with-chrono \
-#     --with-filesystem \
-#     --with-random \
-#     --with-system \
-#     toolset=clang-$ARCH_TYPE \
-#     architecture=$ARCH_TYPE \
-#     variant=release \
-#     target-os=android \
-#     threading=multi \
-#     threadapi=pthread \
-#     link=static \
-#     runtime-link=static \
-
-# cd ..
-# echo "Running ranlib on libraries..."
-# libs=$(find "$TOOLCHAIN/build/lib/" -name '*.a')
-# for lib in $libs; do
-#   "$TOOLCHAIN/bin/$RANLIB_NAME" "$lib"
-# done
-
-# echo "Build boost_$BOOST_VERSION success"
 
 cd $LIBTORRENT_DIR
 
 echo "Building libtorrent_$LIBTORRENT_VERSION"
 
-export PATH=$TOOLCHAIN/bin:$PATH
-export CC=clang
-export CXX=clang++
-
 TORRENT_HOST=arm-linux-androideabi
-TORRENT_PREFIX=${TOOLCHAIN}/build
 
 
 echo "BOOST_PREFIX: ${BOOST_PREFIX}"
 ./configure --host=$TORRENT_HOST \
-            --prefix=$TORRENT_PREFIX \
+            --prefix=${ROOT_DIR}/build/${ARCH_TYPE} \
             --with-boost=${BOOST_PREFIX}/include/boost-1_69 \
             --with-boost-libdir=${BOOST_PREFIX}/lib \
 			--enable-examples=no \
@@ -121,10 +89,8 @@ echo "BOOST_PREFIX: ${BOOST_PREFIX}"
 			--enable-tests=no \
 		    --enable-shared=no \
             --enable-static=yes \
-            --enable-debug=no \
-            --enable-loggin-yes
+            --enable-debug=no 
+            # --enable-loggin-yes
 
 make -j8
 make install
-
-
